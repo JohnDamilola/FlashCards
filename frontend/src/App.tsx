21ee2a506500cfa4a6640c0cf00b702540393926
JohnDamilola <johndamilola03@gmail.com>
2022-09-28 10:38:01 -0400

    Initial routes setup and packages installation

---
+++ frontend/src/App.tsx
@@ -0,0 +1,33 @@
+import AuthLayout from 'layouts/AuthLayout'
+import HomeLayout from 'layouts/HomeLayout'
+import { useEffect } from 'react'
+import { Route, Routes, useLocation } from 'react-router'
+import { authRoutes, publicRoutes } from './routes'
+
+const App = () => {
+  const location = useLocation()
+
+  useEffect(() => {
+    window.scrollTo(0, 0)
+  }, [location])
+
+  return (
+    <>
+      <Routes>
+        <Route element={<HomeLayout />}>
+          {publicRoutes.map(({ path, element }: any, index: number) => (
+            <Route path={path} element={element} key={index} />
+          ))}
+        </Route>
+
+        <Route element={<AuthLayout />}>
+          {authRoutes.map(({ path, element }: any, index: number) => (
+            <Route path={path} element={element} key={index} />
+          ))}
+        </Route>
+      </Routes>
+    </>
+  )
+}
+
+export default App
---
+++ frontend/src/screens/AuthScreens/Login.tsx
@@ -0,0 +1,10 @@
+const Login = () => {
+
+  return (
+    <div className='login-page'>
+      Login
+    </div>
+  )
+}
+
+export default Login
