2c7a52f22644c1e0e218716eb43a8e9116be7413
aaron278 <112004832+aaron278@users.noreply.github.com>
2022-09-30 23:50:39 -0400

    Add tests for accessing the read only part of the database

---
+++ test/test.js
@@ -0,0 +1,19 @@
+const assert=require('assert');
+const firebase=require('@firebase/testing');
+const MY_PROJECT_ID="";
+describe("Flash Cards",()=>
+{
+    it("Can read items from the read only section",async()=>
+    {
+        const db=firebase.initializeTestApp({projectID:MY_PROJECT_ID}).firestore();
+        const testDoc=db.collection("readonly").doc("testDoc");
+        await firebase.assertSucceeds(testDoc.get());
+    })
+
+    it("Cannot write items to the read only section",async()=>
+    {
+        const db=firebase.initializeTestApp({projectID:MY_PROJECT_ID}).firestore();
+        const testDoc=db.collection("readonly").doc("testDoc");
+        await firebase.assertFails(testDoc.set({testdata:"Hello"}));
+    })
+})
