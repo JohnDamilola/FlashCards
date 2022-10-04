5492c901b46a38965e6740412d8bef2cf481fafd
aaron278 <112004832+aaron278@users.noreply.github.com>
2022-10-04 15:32:04 -0400

    Corrected Syntax Errors

--- test/test.js
+++ test/test.js
@@ -8,12 +8,12 @@ describe("Flash Cards",()=>
         const db=firebase.initializeTestApp({projectID:MY_PROJECT_ID}).firestore();
         const testDoc=db.collection("readonly").doc("testDoc");
         await firebase.assertSucceeds(testDoc.get());
-    })
+    });
 
     it("Cannot write items to the read only section",async()=>
     {
         const db=firebase.initializeTestApp({projectID:MY_PROJECT_ID}).firestore();
         const testDoc=db.collection("readonly").doc("testDoc");
         await firebase.assertFails(testDoc.set({testdata:"Hello"}));
-    })
-})
+    });
+});
