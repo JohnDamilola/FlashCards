590767cfcfe9529bffe68b5bfbcb338ed6c0c9db
aaron278 <112004832+aaron278@users.noreply.github.com>
2022-10-06 22:03:16 -0400

    Update LoginTest.py

--- backend/test/LoginTest.py
+++ backend/test/LoginTest.py
@@ -22,27 +22,27 @@ class TestApp(unittest.TestCase):
     def test_signup_route(self):
         '''Test the index route of our app'''
         response=self.app.get('/signup')
-        print(Signup Get, response.status_code)
+        print("Signup Get", response.status_code)
         assert response.status_code==200
         assert response.data.decode('utf-8')=='Testing, Flask!'
 
     def test_index_route_post(self):
         '''Test that the post request to the '/' route is not allowed'''
         response=self.app.post('/signup')
-        print(Signup Post, response.status_code)
+        print("Signup Post", response.status_code)
         assert response.status_code==200
 
     def test_login_route(self):
         '''Test the index route of our app'''
         response=self.app.get('/login')
-        print(Login Get, response.status_code)
+        print("Login Get", response.status_code)
         assert response.status_code==200
         assert response.data.decode('utf-8')=='Testing, Flask!'
 
     def test_login_route_post(self):
         '''Test that the post request to the '/' route is not allowed'''
         response=self.app.post('/login')
-        print(Login Post, response.status_code)
+        print("Login Post", response.status_code)
         assert response.status_code==200
 
 
