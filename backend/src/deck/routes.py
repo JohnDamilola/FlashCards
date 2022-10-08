286a40e9268ce983d9f7eeaa01c715b56ce04cd9
aaron278 <112004832+aaron278@users.noreply.github.com>
2022-10-08 16:18:58 -0400

    Update routes.py

--- backend/src/deck/routes.py
+++ backend/src/deck/routes.py
@@ -2,7 +2,10 @@
 
 from flask import Blueprint, jsonify, request
 from flask_cors import cross_origin
-from .. import firebase
+import sys
+sys.path.append("..")
+#from .. import firebase
+from __init__ import firebase
 
 deck_bp = Blueprint(
     'deck_bp', __name__
