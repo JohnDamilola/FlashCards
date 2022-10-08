987dcf80b506e2ee054c1b2f30c8ad62f06b535f
aaron278 <112004832+aaron278@users.noreply.github.com>
2022-10-08 16:06:25 -0400

    Update routes.py

--- backend/src/auth/routes.py
+++ backend/src/auth/routes.py
@@ -2,9 +2,10 @@ from flask import Blueprint, jsonify
 from flask import current_app as app
 from flask_cors import cross_origin
 from flask import request
-import sys
-sys.path.append("..")
-from .. import firebase
+#import sys
+#sys.path.append("..")
+#from .. import firebase
+import firebase
 
 auth_bp = Blueprint(
     'auth_bp', __name__
