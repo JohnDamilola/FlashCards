c6b8471ccea3e902596ae39b09c0892a0e8cb501
aaron278 <112004832+aaron278@users.noreply.github.com>
2022-10-09 16:58:17 -0400

    Update api.py

--- backend/src/api.py
+++ backend/src/api.py
@@ -1,15 +1,14 @@
 from flask import Flask
 from flask_cors import CORS
-import sys
 
 def create_app():
     """Create Flask application."""
     app = Flask(__name__, instance_relative_config=False)
 
     with app.app_context():
-        from auth.routes import auth_bp
-        from deck.routes import deck_bp
-        from cards.routes import card_bp
+        from .auth.routes import auth_bp
+        from .deck.routes import deck_bp
+        from .cards.routes import card_bp
 
         # Register Blueprints
         app.register_blueprint(auth_bp)
