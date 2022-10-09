24c5bd36e6f73b6d94dba7967b9c78254f85e059
aaron278 <112004832+aaron278@users.noreply.github.com>
2022-10-09 14:29:04 -0400

    Update AuthTest.py

--- backend/test/AuthTest.py
+++ backend/test/AuthTest.py
@@ -18,18 +18,32 @@ class TestApp(unittest.TestCase):
         response=self.app.post('/')
         assert response.status_code==405
 
+    """
     def test_signup_route(self):
         '''Test the signup route of our app'''
         response=self.app.post('/signup',data=dict(email='test@gmail.com',password='testpassword'))
         print('signup',response.status_code)
         assert response.status_code==201
+    """
         
-    def test_login_route(self):
-        '''Test the login route of our app'''
-        response=self.app.post('/login',data=dict(email='test@gmail.com',password='testpassword'))
-        print('login',response.status_code)
+    def test_login_registered_user_route(self):
+        '''Test the login route of our app with an already registered user'''
+        response=self.app.post('/login',data=dict(email='aaronadb@gmail.com',password='flashcards123'))
+        #print('login',response.status_code)
         assert response.status_code==200
-   
+        
+    def test_login_wrong_user_route(self):
+        '''Test the login route of our app with a registered user with a wrong password'''
+        response=self.app.post('/login',data=dict(email='aaronadb@gmail.com',password='flashcards'))
+        #print('login',response.status_code)
+        assert response.status_code==400
+        
+    def test_login_unregistered_user_route(self):
+        '''Test the login route of our app with an unregistered user'''
+        response=self.app.post('/login',data=dict(email='aarondiasbarreto@gmail.com',password='flashcards123'))
+        print('login',response.status_code)
+        assert response.status_code==400
+        
 
 if __name__=="__main__":
     unittest.main()
