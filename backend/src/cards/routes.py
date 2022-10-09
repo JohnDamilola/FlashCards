7d7d2f16dd2273b7a21f9865e123f9833e4e4f17
aaron278 <112004832+aaron278@users.noreply.github.com>
2022-10-09 16:56:41 -0400

    Update routes.py

--- backend/src/cards/routes.py
+++ backend/src/cards/routes.py
@@ -2,9 +2,10 @@
 
 from flask import Blueprint, jsonify, request
 from flask_cors import cross_origin
-import sys
-sys.path.append("..")
-from __init__ import firebase
+try:
+    from .. import firebase
+except ImportError:
+    from __init__ import firebase
 
 card_bp = Blueprint(
     'card_bp', __name__
