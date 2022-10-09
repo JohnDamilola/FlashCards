2d985959ce6a81e788b70fb94f3c51d054e4f27b
aaron278 <112004832+aaron278@users.noreply.github.com>
2022-10-09 01:28:27 -0400

    Update AuthTest.py

--- backend/test/AuthTest.py
+++ backend/test/AuthTest.py
@@ -10,26 +10,22 @@ class TestApp(unittest.TestCase):
     def test_index_get_route(self):
         '''Test the index route of our app'''
         response=self.app.get('/')
-        print("index/get",response.status_code)
         assert response.status_code==200
     
 
     def test_index_post(self):
         '''Test that the post request to the index route is not allowed'''
         response=self.app.post('/')
-        print("index/post",response.status_code)
         assert response.status_code==405
 
     def test_signup_route(self):
         '''Test the signup route of our app'''
         response=self.app.post('/signup')
-        print("sigunp",response.status_code)
         assert response.status_code==200
         
     def test_login_route(self):
         '''Test the login route of our app'''
         response=self.app.post('/login')
-        print("login",response.status_code)
         assert response.status_code==200
    
 
