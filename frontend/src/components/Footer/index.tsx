1e071b9c712c88faf07b2c23f7db2927117c2ce8
JohnDamilola <johndamilola03@gmail.com>
2022-10-09 21:10:23 -0400

    Add copyrights text

--- frontend/src/components/Footer/index.tsx
+++ frontend/src/components/Footer/index.tsx
@@ -5,13 +5,16 @@ const Footer = () => {
     <footer className='footer'>
       <div className='container'>
         <div className='row align-items-center'>
-          <div className='col-lg-8'>
+          <div className='col-lg-6'>
             <div className='footer-text-p'>
               FlashCards{' '}
               <span>
                 / A spaced repetition learning platform.
               </span>
             </div>
+          </div>
+          <div className='col-lg-6 footer-text-p text-right'>
+            <span>Copyright © 2022. All rights reserved.</span>
           </div>
         </div>
       </div>
