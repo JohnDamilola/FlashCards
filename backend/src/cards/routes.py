666094eea070edf6f3f76b700c90fa3b9f29883a
aaron278 <112004832+aaron278@users.noreply.github.com>
2022-10-09 21:49:25 -0400

    Update routes.py

--- backend/src/cards/routes.py
+++ backend/src/cards/routes.py
@@ -22,7 +22,7 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 """
-#routes.py is a file in cards folder that has all the functions defined that manipulate the cards. All CRUD functions that needs to be performed on cards are defined here.
+'''routes.py is a file in cards folder that has all the functions defined that manipulate the cards. All CRUD functions that needs to be performed on cards are defined here.'''
 from flask import Blueprint, jsonify, request
 from flask_cors import cross_origin
 try:
@@ -36,7 +36,7 @@ card_bp = Blueprint(
 
 db = firebase.database()
 
-#This method is called when the user want to fetch all of the cards in a deck. Only the deckid is required to fetch all cards from the required deck.
+'''This method is called when the user want to fetch all of the cards in a deck. Only the deckid is required to fetch all cards from the required deck.'''
 @card_bp.route('/deck/<deckId>/card/all', methods = ['GET'])
 @cross_origin(supports_credentials=True)
 def getcards(deckId):
@@ -55,8 +55,8 @@ def getcards(deckId):
             status = 400
         ), 400
 
-#This method is routed when the user requests to create new cards in a deck. 
-#Only the deckid is required to add cards to a deck.
+'''This method is routed when the user requests to create new cards in a deck. 
+Only the deckid is required to add cards to a deck.'''
 @card_bp.route('/deck/<deckId>/card/create', methods = ['POST'])
 @cross_origin(supports_credentials=True)
 def createcards(deckId):
@@ -65,12 +65,12 @@ def createcards(deckId):
         localId = data['localId']
         cards = data['cards']
         
-        # remove existing cards
+        '''remove existing cards'''
         previous_cards = db.child("card").order_by_child("deckId").equal_to(deckId).get()
         for card in previous_cards.each():
             db.child("card").child(card.key()).remove()
         
-        # add new cards
+        '''add new cards'''
         for card in cards:
             db.child("card").push({
                 "userId": localId,
@@ -90,8 +90,8 @@ def createcards(deckId):
             status = 400
         ), 400
 
-#This method is called when the user requests to update cards in a deck. The card can be updated in terms of its word and meaning.
-#Here deckid and cardid is required to uniquely identify a updating card.
+'''This method is called when the user requests to update cards in a deck. The card can be updated in terms of its word and meaning.
+Here deckid and cardid is required to uniquely identify a updating card.'''
 @card_bp.route('/deck/<id>/update/<cardid>', methods = ['PATCH'])
 @cross_origin(supports_credentials=True)
 def updatecard(id,cardid):
@@ -116,7 +116,7 @@ def updatecard(id,cardid):
             status = 400
         ), 400
  
-#This method is called when the user requests to delete the card. The deckid and the particular cardid is required to delete the card.
+'''This method is called when the user requests to delete the card. The deckid and the particular cardid is required to delete the card.'''
 @card_bp.route('/deck/<id>/delete/<cardid>', methods = ['DELETE'])
 @cross_origin(supports_credentials=True)
 def deletecard(id,cardid):
