3ca7d958bbeabdb72551a9bfa9a32189bf15bb94
aaron278 <112004832+aaron278@users.noreply.github.com>
2022-10-09 21:51:59 -0400

    Update api.py

--- backend/src/api.py
+++ backend/src/api.py
@@ -25,7 +25,7 @@ from flask import Flask
 from flask_cors import CORS
 
 def create_app():
-    """Create Flask application."""
+    '''Create Flask application.'''
     app = Flask(__name__, instance_relative_config=False)
 
     with app.app_context():
