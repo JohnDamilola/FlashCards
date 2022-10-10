a2aecc81406d2861120252e3fab9d8e50c995ea4
aaron278 <112004832+aaron278@users.noreply.github.com>
2022-10-09 23:02:39 -0400

    Update DeckTest.py

--- backend/test/DeckTest.py
+++ backend/test/DeckTest.py
@@ -7,7 +7,7 @@ from auth.routes import auth_bp
 from deck.routes import deck_bp
 from cards.routes import card_bp
 
-class TestApp(unittest.TestCase):
+class DeckTestApp(unittest.TestCase):
     def setUp(self):
         self.app=Flask(__name__, instance_relative_config=False)
         self.app.register_blueprint(deck_bp)
